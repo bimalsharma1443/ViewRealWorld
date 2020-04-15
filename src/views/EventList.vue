1699ca104a5e1d1546061c06a71e14e8b799dd0f
Bimal Sharma <Administrator@SSLBCGWC0R16155.SBICAPSECU.COM>
2020-04-15 16:46:25 +0530

    vue componets with scoped css

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
     <h1>Event List</h1>
-    <p>
-      <router-link :to="{ name: 'EventShow', params:{ id : 1 } }">Event Show #1</router-link>
-    </p>
+    <EventCard/>
   </div>
 </template>
+<script>
+
+import EventCard from '@/components/EventCard.vue';
+
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
