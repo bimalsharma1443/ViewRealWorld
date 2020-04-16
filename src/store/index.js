aedfe78d1c37522193992ba692a3fa73c49b74b6
Bimal Sharma <Administrator@SSLBCGWC0R16155.SBICAPSECU.COM>
2020-04-16 16:19:45 +0530

    implements a getters

--- src/store/index.js
+++ src/store/index.js
@@ -6,12 +6,30 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     user: {id:'1',name:'Bimal Sharma'},
-    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community']
+    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'],
+    todo: [
+      {id:1,name:"...",done:true},
+      {id:2,name:"...",done:false},
+      {id:3,name:"...",done:false},
+      {id:4,name:"...",done:true},
+      
+    ]
   },
   mutations: {},
   actions: {},
   modules: {},
   getters: {
-
+    catLength:state=>{
+      return state.categories.length;
+    },
+    doneTodo:state => {
+      return state.todo.filter(todo => todo.done)
+    },
+    notActiveTodoCount: (state,getters) => {
+      return state.todo.length - getters.doneTodo.length;
+    },
+    getTodoById: (state) => (id) => {
+      return state.todo.find(todo => todo.id === id)
+    } 
   }
 });
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -5,6 +5,13 @@
         <ul>
             <li v-for="category in categories" :key="category">{{ category }}</li>
         </ul>
+        <p>category length is {{ catLength }}</p>
+        <p>List of active todo</p>
+        <ul>
+            <li v-for="todo in doneTodo" :key="todo.id">{{ todo }}</li>
+        </ul>
+        <p>Not active count of todo  is {{ notActiveTodoCount }}</p>
+        <p>Displaying a todo {{ getTodoById(1) }}</p>
     </div>
 </template>
 
@@ -14,6 +21,18 @@ import {mapState} from 'vuex'
 
 export default {
     computed : {
+        catLength() {
+            return this.$store.getters.catLength;
+        },
+        doneTodo() {
+            return this.$store.getters.doneTodo;
+        },
+        notActiveTodoCount() {
+            return this.$store.getters.notActiveTodoCount;
+        },
+        getTodoById() {
+            return this.$store.getters.getTodoById;
+        },
         ...mapState(['user','categories'])
     }
 }
